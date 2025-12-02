737841b12d7498b2e8faafa7d1782bbd6c8a3cf5
yune22 <wjddbs020604@gmail.com>
2025-12-02 17:10:32 +0900

    commit

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -7,3 +7,20 @@ git add .
 # 특정 파일만 add할 경우(경로까지 포함)
 git add test_folder/test1.txt
 
+# commit을 통해 메시지 타이틀과 메시지 내용을 커밋이력으로 생성
+git commit -m "메시지제목" -m "메시지내용"
+# git commit만 입력하고 엔터시 vi 모드 -> 첫 줄: 타이틀, 두번째 줄부터 내용 -> :wq 저장 후 종료시 커밋ID 생성
+git commit
+
+# commit 이력 확인
+git log
+git log --oneline
+
+# head 하단의 로그만 보이는 것이 아니라, 모든 commit 이력을 조회
+# head : 현재 사용(checkout) 하고 있는 commitID 의미
+git log --all
+
+# 원격 저장소로 업로드
+git push origin 브랜치명
+# 충돌 발생시 충돌 무시하고, 로컬 기준의 코드를 원격에 덮어쓰기 (절대 사용하지 말 것)
+git push origin 브랜치명 -- force
