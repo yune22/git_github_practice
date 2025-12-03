7b46d00cac10b7f55e41f534ca301ce9d2bb12d6
yune22 <wjddbs020604@gmail.com>
2025-12-03 14:26:17 +0900

    commit

--- git_syntax/2.basic_command.sh
+++ git_syntax/2.basic_command.sh
@@ -47,3 +47,12 @@ git reset head^
 # 이미 push commit 사항을 되돌리는 새로운 commit 생성
 # commit 지워지지 않음xxx
 git revert 커밋ID
+
+# 작업중인사항을 임시저장
+git stash 
+# 임시저장한 작업사항을 다시 적용
+git stash pop
+# 저장한 작업목록 조회
+git stash list
+# 저장한 목록 전체 삭제
+git stash clear
